515aaae9fb67544fd26752b6f0076b030b67ef5c
Matthew Thompson <matthew.thompson@nasa.gov>
2021-07-07 14:24:30 +0000

    Updates for run-esmf-testing script

--- bash/run-esmf-testing.bash
+++ bash/run-esmf-testing.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-set -Eeuxo pipefail
+set -Eeuo pipefail
+#set -x
 
 # From http://stackoverflow.com/a/246128/1876449
 # ----------------------------------------------
@@ -21,10 +22,15 @@ source $ENV_FILE
 DRYRUN=FALSE
 VERBOSE=FALSE
 
-OS_VERSION="UNKNOWN"
+OS_VERSION=UNKNOWN
 ESMF_VERSION=
 
+COMPILER=UNKNOWN
+
 ORIG_BASELIBS_VERSION=${BASELIBS_VERSION}
+
+DEFAULT_DOCKER_REPO='gmao'
+DOCKER_REPO=${DEFAULT_DOCKER_REPO}
 
 usage () {
    cat << HELP_USAGE
@@ -32,13 +38,19 @@ usage () {
 
    REQUIRED:
       -o <osversion>|--os-version=<osversion>
-         OS version to build (REQUIRED. Allowed values: ubuntu20, opensuse15)
+         OS version to build (REQUIRED. Allowed values: ubuntu20, opensuse15, centos8)
+      --compiler=<compiler>
+         compiler to use (REQUIRED. Allowed values: intel, gnu)
 
    VERSION OPTIONS:
       --baselibs-version=<tag>
          Tag of Baselibs to checkout (Default: ${BASELIBS_VERSION})
       --esmf-version=<tag>
          Tag of ESMF submodule to checkout in Baselibs (Default: Tag in Baselibs being built)
+
+   DOCKER OPTIONS:
+      --docker-repo=<repo>
+         Docker Repository to push to (Default: ${DOCKER_REPO})
 
    OTHER OPTIONS:
       -h|--help
@@ -59,9 +71,12 @@ while getopts hno:v-: OPT; do
   fi
   case "$OPT" in
     o | os-version       ) needs_arg; OS_VERSION="$OPTARG"       ;;
+        compiler         ) needs_arg; COMPILER="$OPTARG"         ;;
 
         baselibs-version ) needs_arg; BASELIBS_VERSION="$OPTARG" ;;
         esmf-version     ) needs_arg; ESMF_VERSION="$OPTARG"     ;;
+
+        docker-repo      ) needs_arg; DOCKER_REPO="$OPTARG"      ;;
 
     h | help     ) usage; exit  ;;
     n | dry-run  ) DRYRUN=TRUE  ;;
@@ -72,6 +87,11 @@ while getopts hno:v-: OPT; do
   esac
 done
 shift $((OPTIND-1)) # remove parsed options and args from $@ list
+
+if [[ "$VERBOSE" == "TRUE" ]]
+then
+   set -x
+fi
 
 # Check we have a valid os
 if [[ "$OS_VERSION" == "UNKNOWN" ]]
@@ -87,8 +107,38 @@ elif [[ "$OS_VERSION" == "ubuntu20" ]]
 then
    BASE_IMAGE="ubuntu:20.04"
    OS_DOCKER_DIR="Ubuntu20"
+elif [[ "$OS_VERSION" == "centos8" ]]
+then
+   BASE_IMAGE="centos:8"
+   OS_DOCKER_DIR="Centos8"
 else
    echo "Invalid osversion!"
+   usage
+   exit 1
+fi
+
+# Check we have a valid compiler
+if [[ "$COMPILER" == "UNKNOWN" ]]
+then
+   echo "compiler must be passed in!"
+   usage
+   exit 1
+elif [[ "$COMPILER" == "intel" ]]
+then
+   COMPILER_NAME="intel"
+   COMPILER_VERSION=${INTEL_VERSION}
+   MPI_NAME="intelmpi"
+   MPI_VERSION=${INTEL_VERSION}
+   FINAL_DOCKER_IMAGE_NAME="geos-env"
+elif [[ "$COMPILER" == "gnu" ]]
+then
+   COMPILER_NAME="gcc"
+   COMPILER_VERSION=${GCC_VERSION}
+   MPI_NAME="openmpi"
+   MPI_VERSION=${OPENMPI_VERSION}
+   FINAL_DOCKER_IMAGE_NAME="geos-env-mkl"
+else
+   echo "Invalid compiler!"
    usage
    exit 1
 fi
@@ -109,6 +159,7 @@ then
    echo "  OS_VERSION: ${OS_VERSION}"
    echo "  BASE_IMAGE: ${BASE_IMAGE}"
    echo "  GCC_VERSION: ${GCC_VERSION}"
+   echo "  INTEL_VERSION: ${INTEL_VERSION}"
    echo "  OPENMPI_VERSION: ${OPENMPI_VERSION}"
    echo "  BASELIBS_VERSION: ${BASELIBS_VERSION}"
    echo "  FV3_VERSION: ${FV3_VERSION}"
@@ -120,9 +171,9 @@ then
    exit
 fi
 
-bash ${PARENTDIR}/build_full_stack.bash --os-version=${OS_VERSION} --build-bsl --no-cache ${EXTRA_OPTIONS} 2>&1 | tee esmf_testing_build_full_stack.out
+bash ${PARENTDIR}/build_full_stack.bash --os-version=${OS_VERSION} --compiler=${COMPILER} --build-bsl --no-cache ${EXTRA_OPTIONS} 2>&1 | tee esmf_testing_build_full_stack.out
 
 [$(docker ps -a | grep runner)] && docker rm --force runner
-docker run --name runner --env-file=$ENV_FILE -dit gmao/${OS_VERSION}-baselibs:${BASELIBS_VERSION}-openmpi_${OPENMPI_VERSION}-gcc_${GCC_VERSION}
+docker run --name runner --env-file=$ENV_FILE -dit ${DOCKER_REPO}/${OS_VERSION}-baselibs:${BASELIBS_VERSION}-${MPI_VERSION}_${MPI_VERSION}-${COMPILER_NAME}_${COMPILER_VERSION}
 docker exec -t runner bash "/opt/esmf-test-Dockerfile.baselibs.bash"
 docker stop runner
