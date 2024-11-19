f47c1c84483b15279ce68fe083cbe21f616dbc60
Matthew Thompson <matthew.thompson@nasa.gov>
2024-11-19 17:43:19 +0000

    Update ifx version

--- versions.sh
+++ versions.sh
@@ -7,10 +7,12 @@ OPENMPI_VERSION=5.0.5
 
 # Note Intel 2024.1 apt image seems to have
 # an issue with omp_lib: https://community.intel.com/t5/Intel-Fortran-Compiler/After-upgrading-ifort-no-longer-finds-include-dir-for-OpenMP/m-p/1602472
-INTEL_VERSION=2024.2
-IFX_VERSION=2024.2
+
+# NOTE NOTE Intel ifort is now dead!
+INTEL_VERSION=2025.0
+IFX_VERSION=2025.0
 IFORT_VERSION=2021.13
-INTELMPI_VERSION=2021.13
+INTELMPI_VERSION=2021.14
 
 BASELIBS_VERSION='v7.27.0'
 
