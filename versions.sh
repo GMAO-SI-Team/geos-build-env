70fb6c6ac05d49d9350776629758c7e182c5382a
Matthew Thompson <matthew.thompson@nasa.gov>
2025-05-06 17:47:09 +0000

    Update to ifx 2025.1. Remove mepoversion

--- versions.sh
+++ versions.sh
@@ -9,12 +9,10 @@ OPENMPI_VERSION=5.0.5
 # an issue with omp_lib: https://community.intel.com/t5/Intel-Fortran-Compiler/After-upgrading-ifort-no-longer-finds-include-dir-for-OpenMP/m-p/1602472
 
 # NOTE NOTE Intel ifort is now dead! The versions are hardcoded in the build script
-INTEL_VERSION=2025.0
-INTELMPI_VERSION=2021.14
+INTEL_VERSION=2025.1
+INTELMPI_VERSION=2021.15
 
 BASELIBS_VERSION='v8.14.0'
-
-MEPO_VERSION='v1.52.0'
 
 FV3_VERSION='v2.9.0'
 GCM_VERSION='v12.0.0'
