585924553d234837691c3cafb34cc19569614e3b
Matthew Thompson <matthew.thompson@nasa.gov>
2025-12-15 18:04:19 +0000

    Update versions

--- versions.sh
+++ versions.sh
@@ -2,8 +2,8 @@
 
 CMAKE_VERSION=3.30.5
 
-GCC_VERSION=14.2.0
-#GCC_VERSION=15.2.0
+#GCC_VERSION=14.2.0
+GCC_VERSION=15.2.0
 OPENMPI_VERSION=5.0.5
 
 #GCC_VERSION=15.1.0
@@ -13,10 +13,10 @@ OPENMPI_VERSION=5.0.5
 # an issue with omp_lib: https://community.intel.com/t5/Intel-Fortran-Compiler/After-upgrading-ifort-no-longer-finds-include-dir-for-OpenMP/m-p/1602472
 
 # NOTE NOTE Intel ifort is now dead! The versions are hardcoded in the build script
-INTEL_VERSION=2025.1
-INTELMPI_VERSION=2021.15
+INTEL_VERSION=2025.2
+INTELMPI_VERSION=2021.16
 
-BASELIBS_VERSION='v8.19.0'
+BASELIBS_VERSION='v9.0.0'
 
 #BCS_VERSION='v11.6.0'
 BCS_VERSION='v12.0.0'
